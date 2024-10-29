2d840883454d465c188343ed5623512fda0a32f2
Valerii Bondariev <valera.bondarev2005@gmail.com>
2024-10-29 23:29:17 +0100

    added simple admin handlers

--- aiogram_run.py
+++ aiogram_run.py
@@ -2,12 +2,14 @@ import asyncio
 from create_bot import bot, dp
 from create_bot import init_db
 from handlers.user_router import user_router
+from handlers.admin_router import admin_router
 
 
 async def main():
     await init_db()
 
     dp.include_router(user_router)
+    dp.include_router(admin_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
--- handlers/admin_router.py
+++ handlers/admin_router.py
@@ -2,8 +2,15 @@ import asyncio
 from aiogram import Router, F
 from aiogram.filters import CommandStart, CommandObject, Command
 from aiogram.types import Message
-from create_bot import bot, channel_id, bot_username
-from keyboards.kbs import main_kb
+from create_bot import bot, admins
+from keyboards.kbs import main_kb, admin_panel_kb
 from aiogram.utils.chat_action import ChatActionSender
 
 admin_router = Router()
+
+
+@admin_router.message((F.text.endswith('Admin panel')) & (F.from_user.id.in_(admins)))
+async def admin_panel(message: Message):
+    async with ChatActionSender.typing(bot=bot, chat_id=message.from_user.id):
+        response_text = "admin panel"
+        await message.answer(text=response_text, reply_markup=admin_panel_kb())
