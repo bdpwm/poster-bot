4593ca9ef06499663944201aa509794216991966
Valerii Bondariev <valera.bondarev2005@gmail.com>
2024-11-01 23:08:41 +0100

    suggested posts view admin handler added

--- db_handlers/db.py
+++ db_handlers/db.py
@@ -2,6 +2,7 @@ from create_bot import AsyncSessionLocal
 from sqlalchemy.ext.asyncio import AsyncSession
 from db_handlers.models import Post
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.future import select
 
 async def save_post(user_id: int, content: str, photo_path: str = None) -> bool:
     async with AsyncSessionLocal() as session:
@@ -14,3 +15,18 @@ async def save_post(user_id: int, content: str, photo_path: str = None) -> bool:
         except SQLAlchemyError as e:
             await session.rollback()
             return False
+
+
+
+
+async def get_post() -> Post | None:
+    async with AsyncSessionLocal() as session:
+        try:
+            result = await session.execute(
+                select(Post).filter(Post.status == 'pending')
+            )
+            post = result.scalars().first()
+            return post
+        except SQLAlchemyError as e:
+            print(f"Error loading post: {e}")
+            return None
--- handlers/admin_router.py
+++ handlers/admin_router.py
@@ -2,9 +2,12 @@ import asyncio
 from aiogram import Router, F
 from aiogram.filters import CommandStart, CommandObject, Command
 from aiogram.types import Message
+from aiogram.types import FSInputFile
 from create_bot import bot, admins
-from keyboards.kbs import main_kb, admin_panel_kb
+from keyboards.kbs import main_kb, admin_panel_kb, sug_posts_kb
+from db_handlers.db import get_post
 from aiogram.utils.chat_action import ChatActionSender
+
 
 admin_router = Router()
 
@@ -14,3 +17,18 @@ async def admin_panel(message: Message):
     async with ChatActionSender.typing(bot=bot, chat_id=message.from_user.id):
         response_text = "admin panel"
         await message.answer(text=response_text, reply_markup=admin_panel_kb())
+
+@admin_router.message((F.text.endswith('Suggested Posts')) & (F.from_user.id.in_(admins)))
+async def suggested_posts(message: Message):
+    async with ChatActionSender.typing(bot=bot, chat_id=message.from_user.id):
+        post = await get_post()
+        if post:
+            if post.photo_path:
+                # add more options
+                    photo = FSInputFile(post.photo_path)
+                    await message.answer_photo(photo=photo, caption=post.content, reply_markup=sug_posts_kb())
+            else:
+                await message.answer(text=post.content, reply_markup=sug_posts_kb())
+        else:
+            response_text = "No available posts."
+            await message.answer(text=response_text, reply_markup=sug_posts_kb())
